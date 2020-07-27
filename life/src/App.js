98ddaa27a585c744f0ec072ae168ccd7682c46ab
carlsachs <sachscarl@gmail.com>
2020-07-27 15:24:30 -0400

    got simulation state set up

--- life/src/App.js
+++ life/src/App.js
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import './App.css';
+
+//import other libraries
+import produce from "immer";
 
 function App() {
 
@@ -15,30 +18,54 @@ function App() {
     return rows;
   });
 
-  console.log(grid)
+  const [running, setRunning] = useState(false);
+  
+  const runRef = useRef(running);
+  runRef.current = running
+
+  const simulation = useCallback(() => {
+    if (!runRef.current) {
+      return;
+    }
+
+    setTimeout(simulation, 850);
+  }, [])
 
   return (
-    <div style={{
-      display: 'grid',
-      gridTemplateColumns: `repeat(${numCols}, 30px)`
-    }}>
-    
-    {grid.map((rows, i) => 
-      rows.map((col, q) => 
-      (<div 
-        key={`${i}-${q}`}
-        onClick={() => {
-          
-        }}
-        style={{
-          height: 30, 
-          width: 30, 
-          backgroundColor: grid[i][q] ? "darkgreen" : undefined,
-          border: "1px solid green",
-    }} />
-    ))
-    )}
-    
+  <div>
+
+      <button
+      onClick={() => {
+        setRunning(!running);
+      }}>
+      {running ? "death" : "birth"}
+      </button>
+
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${numCols}, 30px)`
+      }}>
+      
+      {grid.map((rows, i) => 
+        rows.map((col, q) => 
+        (<div 
+          key={`${i}-${q}`}
+          onClick={() => {
+            const newGrid = produce(grid, gridCopy => {
+              gridCopy[i][q] = grid[i][q] ? 0 : 1;
+            })
+            setGrid(newGrid)
+          }}
+          style={{
+            height: 30, 
+            width: 30, 
+            backgroundColor: grid[i][q] ? "darkgreen" : undefined,
+            border: "1px solid green",
+          }} />
+          ))
+          )}
+      
+      </div>
     </div>
   );
 }
